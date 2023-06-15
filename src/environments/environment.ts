6dd43f2c0033a86134a62550c1ac7b986e1ebcbf
Joaquin Miguel Esquivel Meza <115098686+JoaquinE7@users.noreply.github.com>
2023-06-15 09:47:05 -0600

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment={
+    production:false,
+    firebaseConfig :{
+        apiKey: "AIzaSyCy-OdmQDLglPrwY8nMHFJCGtoHvhre7oo",
+        authDomain: "prueba-firebase-21486.firebaseapp.com",
+        databaseURL: "https://prueba-firebase-21486-default-rtdb.firebaseio.com",
+        projectId: "prueba-firebase-21486",
+        storageBucket: "prueba-firebase-21486.appspot.com",
+        messagingSenderId: "536581687145",
+        appId: "1:536581687145:web:aea8f2d8c081bca27fdff8",
+        measurementId: "G-BDZ2Z3VC6J"
+    }
+};
